088fbd80fedff8af75f838e803ecdb1b1d6d064d
Yashaswi <68151261+yashaswi6991@users.noreply.github.com>
2020-12-11 13:07:22 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,9 @@
+/*
+program to perform calculations
+between two numbers 
+by creating objects and calling the methods
+*/
+
 public class Calculator
 {
 
@@ -5,36 +11,51 @@ public class Calculator
   {
 
   }
-  //addiction
+  //adding two variables a and b
   public int add(int a,int b)
   {
-    
+    //returning the value a+b
     return (a+b);
   
 }
-//substraction
+
+  //substracting two variables a and b
 public int substract(int a,int b)
 {
+  
+    //returning the value a-b
  return (a-b);
 }
+  //multiplying the variables 
  public int multiply(int a, int b)
  {
+   
+    //returning the value a*b
    return a*b;
  }
+  //dividing a and b
  public int divide(int a,int b)
  {
+   
+    //returning the value a/b
    return a/b ;
  }
+  //modulus method
 public int modulo(int a,int b)
 {
+  
+    //returning the value a%b
   return a%b ;
 }
 public static void main(String[] args)
 
 {
+  /*Creating an object and  calling the methods with the instance*/
   Calculator myCalculator = new Calculator();
   System.out.println(myCalculator.add(5,7));
   System.out.println(myCalculator.substract(45,11));
+    System.out.println(myCalculator.multiply(5,5));
+  System.out.println(myCalculator.divide(45,5));
   
 
 }
